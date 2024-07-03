6f36b1466912020060fcf2c13e1caa69e95f5b26
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-07-03 13:36:11 +0300

    Add styles for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
+import styles from './Header.module.scss';
 
 const Header = () => {
-    return (
-        <header>
-            <h1>My counter</h1>
-            <nav>
-                <ul>
-                    <li>link 1</li>
-                    <li>link 2</li>
-                    <li>link 3</li>
-                </ul>
-            </nav>
+  return (
+    <header className={styles.header}>
+      <h1>My Counter</h1>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navListItem}>Link 1</li>
+          <li className={styles.navListItem}>Link 2</li>
+          <li className={styles.navListItem}>Link 3</li>
+        </ul>
+      </nav>
 
-            <div>
-                <button>Switch Theme</button>
-            </div>
-        </header>
-    )
+      <div>
+        <button>Switch Theme</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
